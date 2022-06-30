12507188811d23da89add527e69cafe509524ff1
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-06-30 23:31:07 +0200

    :sparkles: Introducing Preact Router

--- views/App.jsx
+++ views/App.jsx
@@ -1,3 +1,19 @@
+import Router from 'preact-router';
+import { Link } from 'preact-router/match';
+import Home from './Home';
+import About from './About';
+
 export default function App() {
-  return <div>Hello World!</div>
+    return (
+        <>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+            </nav>
+            <Router>
+                <Home path="/" />
+                <About path="/about" />
+            </Router>
+        </>
+    );
 }
