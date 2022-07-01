f1fd6104186aab1e1ef1292aa43a51c71f5df499
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-07-01 22:04:27 +0200

    :truck: Renaming main.jsx to main.js

--- build.js
+++ build.js
@@ -1,23 +1,23 @@
-const glob = require("glob");
-const alias = require("esbuild-plugin-alias");
+const glob = require('glob');
+const alias = require('esbuild-plugin-alias');
 
-require("esbuild").build({
-  entryPoints: ["main.jsx"],
-  bundle: true,
-  minify: true,
-  splitting: true,
-  outdir: "dist",
-  format: "esm",
-  loader: {
-    ".js": "jsx",
-  },
-  inject: ["./preact-shim.js"],
-  jsxFactory: "h",
-  jsxFragment: "Fragment",
-  plugins: [
-    alias({
-      "react-dom": "@preact/compat",
-      react: "@preact/compat",
-    }),
-  ],
+require('esbuild').build({
+    entryPoints: ['main.js'],
+    bundle: true,
+    minify: true,
+    splitting: true,
+    outdir: 'dist',
+    format: 'esm',
+    loader: {
+        '.js': 'jsx',
+    },
+    inject: ['./preact-shim.js'],
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    plugins: [
+        alias({
+            'react-dom': '@preact/compat',
+            react: '@preact/compat',
+        }),
+    ],
 });
